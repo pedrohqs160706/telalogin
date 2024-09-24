0ee4f049486e27acf0c9e12790922ebd4d8d4fe7
PEDRO HENRIQUE QUEIROZ SANTOS <23112113@dsn127m1241953.cfp127aluno.local>
2024-09-24 14:07:10 -0300

    comecando

---
+++ app/src/main/java/br/senai/sp/jandira/vital/screens/TelaInicial1.kt
@@ -0,0 +1,80 @@
+package br.senai.sp.jandira.vital.screens
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.vital.R
+import br.senai.sp.jandira.vital.model.Login
+import br.senai.sp.jandira.vital.model.Usuario
+import br.senai.sp.jandira.vital.service.RetrofitFactory
+import br.senai.sp.jandira.vital.ui.theme.VitalTheme
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+@Composable
+fun TelaInicial1() {
+
+  Column (
+      modifier = Modifier
+          .fillMaxSize()
+          .offset(y = 78.dp),
+      horizontalAlignment = Alignment.CenterHorizontally
+  ){
+      Image(painter =
+      painterResource(id = R.drawable.medica),
+          contentDescription = "",
+          modifier = Modifier
+              .width(0.dp)
+      )
+  }
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TelaInicial1Preview () {
+
+    // Pre-visualizacao
+    VitalTheme {
+        TelaInicial1()
+    }
+
+
+}
